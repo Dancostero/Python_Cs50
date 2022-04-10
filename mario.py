8a27595b418b4f219b7f84736e91c4513616a0ff
Daniel Costero <Danielcostero13@gmail.com>
2022-04-10 19:52:32 -0400

    first try Please# Please# Please# Please# Please# Please# Please# Please# Please   P leasenter the commit message for your changes. Lines starting   k

---
+++ mario.py
@@ -0,0 +1,28 @@
+# def main():
+#     height = get_height()
+#     for i in range(height):
+#         print('#')
+
+# def get_height():
+#     while True:
+#         try:
+#             n = int(input('Height: '))
+#             if n > 0:
+#                 break
+#         except ValueError:
+#             print('Thats not an integer')
+#     return n
+
+# main()
+
+# height = int(input('Height: '))
+
+# for y in range(height):
+#     print('#', end='')
+#     for x in range(height):
+#         print('#', end='')
+#     print(' ')
+dato =1
+for j in range(0,7,+1):
+        print('#'*j)
+
