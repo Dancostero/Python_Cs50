43c08bb84ce9b0240dda5f6ce95605c585651736
Daniel Costero <Danielcostero13@gmail.com>
2022-04-14 17:17:03 -0400

    Cs50 version, still lacking one loop for promt

--- mario.py
+++ mario.py
@@ -1,7 +1,11 @@
 
 
-h = int(input('Height: '))
+from cs50 import get_int
 
-for j in range(h,0,-1):
-        print(f'{" "*(j-1)}{"#"*(h-j+1)} {"#"*(h-j+1)}')
+def main():
+    while h > 0 and h <= 8:
+        h = get_int('Height: ')
+        for j in range(h,0,-1):
+            print(f'{" "*(j-1)}{"#"*(h-j+1)}  {"#"*(h-j+1)}')
 
+main()
